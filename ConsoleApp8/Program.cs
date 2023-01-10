3fa9e2b65691f95f68dd614a788467135b5b2f60
özge alkan <ozgetanulkualkan@gmail.com>
2023-01-10 19:30:35 +0300

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileApplication //https://www.tutorialspoint.com/csharp/csharp_text_files.htm#
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Zara Ali", "Nuha Ali" };
+
+            using (StreamWriter sw = new("names.txt"))
+            {
+
+                foreach (string s in names)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+
+            // Read and show each line from the file.
+            string line = "";
+            using (StreamReader sr = new("names.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
